d004ec5d538cbad1e2506518dc566c181a0780a1
nkduchuy <nkduchuy@yahoo.com.vn>
2024-02-17 18:10:12 -0700

    Auction - Created DTOs and added mapping profiles

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -12,6 +12,9 @@ builder.Services.AddDbContext<AuctionDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+// Add AutoMapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
