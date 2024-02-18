ead497680d08ebb6abc55f4804a4a8aa7e836b9c
nkduchuy <nkduchuy@yahoo.com.vn>
2024-02-17 21:37:36 -0700

    Search - Made HTTP comm to Auction more resilient with retry/poll policy

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,7 +1,7 @@
-using MongoDB.Driver;
-using MongoDB.Entities;
+using System.Net;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService.Data;
-using SearchService.Entities;
 using SearchService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,7 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 // Add HttpClient for calling AuctionService
-builder.Services.AddHttpClient<AuctionServiceHttpClient>();
+builder.Services.AddHttpClient<AuctionServiceHttpClient>().AddPolicyHandler(GetPolicy());
 
 var app = builder.Build();
 
@@ -21,15 +21,24 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-// Initialize MongoDB
-try
+app.Lifetime.ApplicationStarted.Register(async () =>
 {
-    await DbInitializer.InitDb(app);
-}
-catch (Exception ex)
-{
-    Console.WriteLine("Error initializing database", ex);
-    throw;
-}
+    // Initialize MongoDB
+    try
+    {
+        await DbInitializer.InitDb(app);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Error initializing database", ex);
+        throw;
+    }
+});
 
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
