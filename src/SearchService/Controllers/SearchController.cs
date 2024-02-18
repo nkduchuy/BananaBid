bdd1f9cc5ffb57d5e74e9331d9c3921e3810f842
nkduchuy <nkduchuy@yahoo.com.vn>
2024-02-17 20:43:13 -0700

    Search - Added filtering and ordering

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Entities;
 using SearchService.Entities;
+using SearchService.RequestHelpers;
 
 namespace SearchService.Controllers
 {
@@ -9,16 +10,37 @@ namespace SearchService.Controllers
     public class SearchController : ControllerBase
     {
         [HttpGet]
-        public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm, int pageNumber = 1, int pageSize = 4)
+        public async Task<ActionResult<List<Item>>> SearchItems([FromQuery]SearchParams searchParams)
         {
-            var query = DB.PagedSearch<Item>();
+            var query = DB.PagedSearch<Item, Item>();
 
-            query.Sort(x => x.Make, Order.Ascending);
+            if (!string.IsNullOrEmpty(searchParams.SearchTerm))
+                query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();
 
-            if (!string.IsNullOrEmpty(searchTerm))
-                query.Match(Search.Full, searchTerm).SortByTextScore();
+            // Sort results
+            query = searchParams.OrderBy switch
+            {
+                "make" => query.Sort(x => x.Ascending(a => a.Make)),
+                "new" => query.Sort(x => x.Descending(a => a.CreatedAt)),
+                _ => query.Sort(x => x.Ascending(a => a.AuctionEnd))
+            };
 
-            query.PageNumber(pageNumber).PageSize(pageSize);
+            // Filter results by auction time
+            query = searchParams.FilterBy switch
+            {
+                "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
+                "endingSoon" => query.Match(x => x.AuctionEnd > DateTime.UtcNow && x.AuctionEnd < DateTime.UtcNow.AddHours(6)),
+                _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow)
+            };
+
+            // Filter results by seller and/or winner
+            if (!string.IsNullOrEmpty(searchParams.Seller))
+                query.Match(x => x.Seller == searchParams.Seller);
+            if (!string.IsNullOrEmpty(searchParams.Winner))
+                query.Match(x => x.Winner == searchParams.Winner);
+            
+            // Paginate results
+            query.PageNumber(searchParams.PageNumber).PageSize(searchParams.PageSize);
 
             var result = await query.ExecuteAsync();
 
