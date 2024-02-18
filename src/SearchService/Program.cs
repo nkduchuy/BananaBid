972ba5f6f4f61178d6c488c8ddd10b9925610d11
nkduchuy <nkduchuy@yahoo.com.vn>
2024-02-17 19:52:47 -0700

    Search - Created Item entity and setup mongodb connection

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,5 +15,13 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Initialize MongoDB and define indexes for searching
+await DB.InitAsync("SearchDb", MongoClientSettings.FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection")));
+await DB.Index<Item>()
+    .Key(a => a.Make, KeyType.Text)
+    .Key(a => a.Model, KeyType.Text)
+    .Key(a => a.Color, KeyType.Text)
+    .CreateAsync();
 
 app.Run();
