24bd2d7440310d746ac49ccb2fcea85a8b3f26a5
nkduchuy <nkduchuy@yahoo.com.vn>
2024-02-18 19:04:35 -0700

    Auction - Publish newly created auction

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -3,6 +3,8 @@ using AuctionService.DTOs;
 using AuctionService.Entities;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using Contracts;
+using MassTransit;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,11 +16,13 @@ namespace AuctionService.Controllers
     {
         private readonly AuctionDbContext _context;
         private readonly IMapper _mapper;
+        private readonly IPublishEndpoint _publishEndpoint;
 
-        public AuctionsController(AuctionDbContext context, IMapper mapper)
+        public AuctionsController(AuctionDbContext context, IMapper mapper, IPublishEndpoint publishEndpoint)
         {
             _context = context;
             _mapper = mapper;
+            _publishEndpoint = publishEndpoint;
         }
 
         [HttpGet]
@@ -52,8 +56,13 @@ namespace AuctionService.Controllers
             // TODO: Add current user as Seller
             auction.Seller = "test";
 
+            // Save to database
             _context.Auctions.Add(auction);
             var result = await _context.SaveChangesAsync() > 0;
+
+            // Map and publish to RabbitMQ
+            var newAuction = _mapper.Map<AuctionDto>(auction);
+            await _publishEndpoint.Publish(_mapper.Map<AuctionCreated>(newAuction));
 
             if (!result)
                 return BadRequest("Could not save auction to database");
