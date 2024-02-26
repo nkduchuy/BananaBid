f7407d7d98e38403f8aa7c3358fb66186cebf9b6
nkduchuy <nkduchuy@yahoo.com.vn>
2024-02-26 11:31:02 -0700

    Search - Added event consumers

---
+++ src/SearchService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,26 @@
+﻿using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Entities;
+
+namespace SearchService;
+
+public class AuctionFinishedConsumer : IConsumer<AuctionFinished>
+{
+    public async Task Consume(ConsumeContext<AuctionFinished> context)
+    {
+        // Get auction from db
+        var auction = await DB.Find<Item>().OneAsync(context.Message.AuctionId);
+
+        // If sold, update winner and sold amount
+        if (context.Message.ItemSold)
+        {
+            auction.Winner = context.Message.Winner;
+            auction.SoldAmount = (int)context.Message.Amount;
+        }
+
+        // Update status and save
+        auction.Status = "Finished";
+        await auction.SaveAsync();
+    }
+}
---
+++ src/SearchService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,25 @@
+﻿using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Entities;
+
+namespace SearchService;
+
+public class BidPlacedConsumer : IConsumer<BidPlaced>
+{
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        Console.WriteLine("--> Consuming bid placed event");
+
+        // Get auction from db
+        var auction = await DB.Find<Item>().OneAsync(context.Message.AuctionId);
+
+        // Update high bid if new bid is higher
+        if (context.Message.BidStatus.Contains("Accepted") 
+            && context.Message.Amount > auction.CurrentHighBid)
+        {
+            auction.CurrentHighBid = context.Message.Amount;
+            await auction.SaveAsync();
+        }
+    }
+}
