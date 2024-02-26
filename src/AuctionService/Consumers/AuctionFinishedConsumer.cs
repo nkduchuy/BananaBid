a1b20afb77fa6950878028f8961fe517910b36fd
nkduchuy <nkduchuy@yahoo.com.vn>
2024-02-26 11:24:41 -0700

    Auction - Added  more event consumers

---
+++ src/AuctionService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,38 @@
+using AuctionService.Data;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumers
+{
+    public class AuctionFinishedConsumer : IConsumer<AuctionFinished>
+    {
+        private readonly AuctionDbContext _dbContext;
+
+        public AuctionFinishedConsumer(AuctionDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Consume(ConsumeContext<AuctionFinished> context)
+        {
+            
+            Console.WriteLine("--> Consuming auction finished event");
+
+            // Get auction from db
+            var auction = await _dbContext.Auctions.FindAsync(context.Message.AuctionId);
+
+            // If sold, update winner and sold amount
+            if (context.Message.ItemSold)
+            {
+                auction.Winner = context.Message.Winner;
+                auction.SoldAmount = context.Message.Amount;
+            }
+
+            // Update auction status
+            auction.Status = auction.SoldAmount > auction.ReservePrice
+                ? Entities.Status.Finished : Entities.Status.ReserveNotMet;
+
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ src/AuctionService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,32 @@
+﻿using AuctionService.Data;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService;
+
+public class BidPlacedConsumer : IConsumer<BidPlaced>
+{
+    private readonly AuctionDbContext _dbContext;
+
+    public BidPlacedConsumer(AuctionDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        Console.WriteLine("--> Consuming bid placed event");
+
+        // Get auction from db
+        var auction = await _dbContext.Auctions.FindAsync(context.Message.AuctionId);
+
+        // If no current high bid or new bid is higher, update high bid
+        if (auction.CurrentHighBid == null 
+            || context.Message.BidStatus.Contains("Accepted") 
+            && context.Message.Amount > auction.CurrentHighBid)
+        {
+            auction.CurrentHighBid = context.Message.Amount;
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
