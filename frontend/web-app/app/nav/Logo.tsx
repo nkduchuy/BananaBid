2841889de735cd55e412ec8eb8f9e88da2ed9f3e
nkduchuy <nkduchuy@yahoo.com.vn>
2024-03-03 09:55:27 -0700

    WebApp - Created a dropdown for user actions

--- frontend/web-app/app/nav/Logo.tsx
+++ frontend/web-app/app/nav/Logo.tsx
@@ -1,14 +1,24 @@
 'use client'
 
 import { useParamsStore } from '@/hooks/useParamsStore'
+import { useRouter, usePathname } from 'next/navigation'
 import React from 'react'
 import { LuBanana } from 'react-icons/lu'
 
 export default function Logo() {
+    const router = useRouter();
+    const pathname = usePathname();
     const reset = useParamsStore(state => state.reset);
 
+    function doReset() {
+        if (pathname !== '/')
+            router.push('/');
+
+        reset();
+    }
+
     return (
-        <div onClick={reset} className='cursor-pointer flex items-center gap-2 text-3xl font-semibold text-red-500'>
+        <div onClick={doReset} className='cursor-pointer flex items-center gap-2 text-3xl font-semibold text-red-500'>
             <LuBanana size={34} />
             <div>BananaBid</div>
         </div>
