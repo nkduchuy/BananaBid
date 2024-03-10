9dc1641b43fb274835ba0cbbd273b9050a422dc8
nkduchuy <nkduchuy@yahoo.com.vn>
2024-03-09 18:33:18 -0700

    WebApp - Fixed error with missing attributes

--- frontend/web-app/app/session/page.tsx
+++ frontend/web-app/app/session/page.tsx
@@ -9,7 +9,7 @@ export default async function Session() {
 
     return (
         <div>
-            <Heading title='Session dashboard' />
+            <Heading title='Session dashboard' subtitle={''} />
 
             <div className='bg-blue-200 border-2 border-blue-500'>
                 <h3 className='text-lg'>Session data</h3>
