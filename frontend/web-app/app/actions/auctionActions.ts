c1a47bbd81c3751280f24de5f13c8a7c24e1cbd7
nkduchuy <nkduchuy@yahoo.com.vn>
2024-03-12 20:12:38 -0600

    WebApp - Added update auction functionality

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -4,6 +4,7 @@ import { Auction, PagedResult } from "@/types";
 import { getTokenWorkaround } from "./authActions";
 import { fetchWrapper } from "@/lib/fetchWrapper";
 import { FieldValues } from "react-hook-form";
+import { revalidatePath } from "next/cache";
 
 export async function getData(query: string) : Promise<PagedResult<Auction>> {
     return await fetchWrapper.get(`search${query}`);
@@ -23,4 +24,10 @@ export async function createAuction(data: FieldValues) {
 
 export async function getDetailsViewData(id: string): Promise<Auction> {
     return await fetchWrapper.get(`auctions/${id}`);
+}
+
+export async function updateAuction(data: FieldValues, id: string) {
+    const res = await fetchWrapper.put(`auctions/${id}`, data);
+    revalidatePath(`/auctions/details/${id}`);
+    return res;
 }
