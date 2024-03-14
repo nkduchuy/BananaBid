b90d884d38661eed42f6231331e27b762b4a6b1d
nkduchuy <nkduchuy@yahoo.com.vn>
2024-03-13 18:38:14 -0600

    WebApp - Added delete auction functionality

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -31,3 +31,7 @@ export async function updateAuction(data: FieldValues, id: string) {
     revalidatePath(`/auctions/details/${id}`);
     return res;
 }
+
+export async function deleteAuction(id: string) {
+    return await fetchWrapper.del(`auctions/${id}`);
+}
