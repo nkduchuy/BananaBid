4c4bdf7e9acb6c0e79b0bbd30425bba2728018fd
nkduchuy <nkduchuy@yahoo.com.vn>
2024-06-10 18:29:35 -0600

    Auction - Added Int Tests for GetAuctions & GetAuctionById endpoints

---
+++ tests/AuctionService.IntegrationTests/AuctionControllerTests.cs
@@ -0,0 +1,81 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using AuctionService.Data;
+using AuctionService.DTOs;
+using AuctionService.IntegrationTests.Fixtures;
+using AuctionService.IntegrationTests.Util;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuctionService.IntegrationTests;
+
+public class AuctionControllerTests : IClassFixture<CustomWebAppFactory>, IAsyncLifetime
+{
+    private readonly CustomWebAppFactory _factory;
+    private readonly HttpClient _httpClient;
+    private const string Bugatti_ID = "c8c3ec17-01bf-49db-82aa-1ef80b833a9f";
+
+    public AuctionControllerTests(CustomWebAppFactory factory)
+    {
+        _factory = factory;
+        _httpClient = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task GetAuctions_ShouldReturn3Auctions()
+    {
+        // Arrange
+
+        // Act
+        var response = await _httpClient.GetFromJsonAsync<List<AuctionDto>>("api/auctions");
+
+        // Assert
+        Assert.Equal(3, response.Count);
+    }
+
+    [Fact]
+    public async Task GetAuctionById_WithValidId_ShouldReturnAuction()
+    {
+        // Arrange
+
+        // Act
+        var response = await _httpClient.GetFromJsonAsync<AuctionDto>($"api/auctions/{Bugatti_ID}");
+
+        // Assert
+        Assert.Equal("Bugatti", response.Make);
+    }
+
+    [Fact]
+    public async Task GetAuctionById_WithInvalidId_Returns404NotFound()
+    {
+        // Arrange
+
+        // Act
+        var response = await _httpClient.GetAsync($"api/auctions/{Guid.NewGuid()}");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task GetAuctionById_WithInvalidGuidReturns400BadRequest()
+    {
+        // Arrange
+
+        // Act
+        var response = await _httpClient.GetAsync("api/auctions/notaguid");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+    }
+
+    public Task InitializeAsync()
+        => Task.CompletedTask;
+    
+    public Task DisposeAsync()
+    {
+        using var scope = _factory.Services.CreateScope();
+        var db = scope.ServiceProvider.GetRequiredService<AuctionDbContext>();
+        DbHelper.ReinitDbForTests(db);
+        return Task.CompletedTask;
+    }
+}
