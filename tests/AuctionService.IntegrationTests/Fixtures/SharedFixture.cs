f0b5134e4a579d560045f7b1e2171212d9d159de
nkduchuy <nkduchuy@yahoo.com.vn>
2024-06-15 21:13:54 -0600

    Auction - Use collection fixtures to share DB between test classes

---
+++ tests/AuctionService.IntegrationTests/Fixtures/SharedFixture.cs
@@ -0,0 +1,9 @@
+﻿using AuctionService.IntegrationTests.Fixtures;
+
+namespace AuctionService.IntegrationTests;
+
+[CollectionDefinition("Shared collection")]
+public class SharedFixture : ICollectionFixture<CustomWebAppFactory>
+{
+
+}
